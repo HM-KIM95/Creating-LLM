9e2d25d50b1c3a21b68b484c1fe958a8c3a5e3b6
NIXKim <dbb04017@gmail.com>
2025-11-09 16:12:37 +0900

    edited the Text_Tokenized Code

--- Text_Tokenized.py
+++ Text_Tokenized.py
@@ -1,6 +1,17 @@
-import urllib.request
-url = ("https://raw.githubusercontent.com/rickiepark/"
-       "llm-from-scratch/main/ch02/01_main-chapter-code"
-       "the-verdict.txt")
-file_path = "the-verdict.txt"
-urllib.request.urlretrieve(url, file_path)
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+        
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = " ",join([self.int_to_str[i] fir i in ids])
+        text = re.sub(r's+([,.?!"()\'])',r'\1', text)
+        return text
